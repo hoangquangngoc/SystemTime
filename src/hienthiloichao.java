10a88195d8bef2b82bbd1f515ad2bcab0b03e0c1
hoangquangngoc <hoangquangngoc10@gmail.com>
2022-09-05 17:47:38 +0700

    add

---
+++ src/hienthiloichao.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hienthiloichao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap ten \t ");
+        String name = sc.nextLine();
+
+                System.out.print("hello" + name);
+
+        }
+    }
+
