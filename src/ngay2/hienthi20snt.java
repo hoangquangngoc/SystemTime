70d6e1b86030ea5047449e0bf8bc75ef29e80ee5
hoangquangngoc <hoangquangngoc10@gmail.com>
2022-09-06 20:35:41 +0700

    add

---
+++ src/ngay2/hienthi20snt.java
@@ -0,0 +1,28 @@
+package ngay2;
+
+import java.util.Scanner;
+
+public class hienthi20snt{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("kiem tra snt");
+        int numbers = sc.nextInt();
+        System.out.println("Display "+numbers + " primes: ");
+        int count = 0;
+        int number =2;
+        while (count<numbers){
+            boolean check = true;
+            for (int i =2 ; i<=Math.sqrt(number);i++){
+                if (number%i==0 && i!=1){
+                    check = false;
+
+                }
+            }
+            if (check){
+                System.out.print(number+"\t");
+                count++;
+            }
+            number++;
+        }
+    }
+}
